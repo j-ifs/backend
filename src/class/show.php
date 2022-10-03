2dd0b15c7338ec9e0c1bf1b5d2b35c46628e2875
Arthur Saldanha <saldanhadev@gmail.com>
2022-10-03 11:19:34 -0300

    ✨ feat: class show endpoint

---
+++ src/class/show.php
@@ -0,0 +1,32 @@
+<?php
+
+    require_once("../database/connect.php");
+
+    $classId = substr($_SERVER["PATH_INFO"], 1);
+
+    $searchSql = "SELECT * FROM turma WHERE id_turma = ?;";
+
+    $databaseConnection = Connect();
+
+    $preapredStatement = $databaseConnection->prepare($searchSql);
+
+    $preapredStatement->bind_param("s", $classId);
+    $preapredStatement->execute();
+
+    $searchQueryResult = $preapredStatement->get_result();
+
+    $classData = $searchQueryResult->fetch_assoc();
+
+    http_response_code(200);
+    header("Content-Type: application/json");
+
+    echo json_encode(
+        [
+            "class" => [
+                "id" => $classData["id_turma"],
+                "course" => $classData["curso"],
+                "year" => $classData["ano"]
+            ]
+        ]
+    );
+?>
