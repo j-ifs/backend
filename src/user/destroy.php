73e23c4dcac6aa97279e07f4c48a9d13bade47d2
padilhaigor <igor.padilha2004@gmail.com>
2022-10-12 12:20:48 -0300

    🩹 fix: delete data for 'representante' role

--- src/user/destroy.php
+++ src/user/destroy.php
@@ -5,6 +5,24 @@ $id_usuario = substr($_SERVER["PATH_INFO"], 1);
 
 $databaseConnection = Connect();
 
+$searchSql = "SELECT id_representante FROM adm WHERE id_adm = ?";
+
+$searchStatement = $databaseConnection->prepare($searchSql);
+$searchStatement->bind_param("i", $id_usuario);
+
+$searchStatement->execute();
+$searchResult = $searchStatement->get_result();
+
+$agentId = null;
+if($searchResult->num_rows) {
+    $searchData = $searchResult->fetch_assoc();
+    $agentId = $searchData["id_representante"];
+
+} else{
+    http_response_code(404);
+    exit();
+}
+
 $deleteSql = "DELETE FROM adm WHERE id_adm = ?;";
 
 
@@ -12,12 +30,15 @@ $deleteStatement = $databaseConnection->prepare($deleteSql);
 
 $deleteStatement->bind_param("i", $id_usuario);
 
-$noErrorOcorred= $deleteStatement->execute();
+$noErrorOccurred= $deleteStatement->execute();
 
-if ($noErrorOcorred) {
+if($agentId && $noErrorOccurred) {
+    $noErrorOccurred = $databaseConnection->query("DELETE FROM representante WHERE id_representante = $agentId");
+}
+
+if ($noErrorOccurred) {
 
     http_response_code(200);
-
 } else {
 
     http_response_code(500);
