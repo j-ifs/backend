45383f82392f8ab87cc5c9b89b2f0209076a0d75
padilhaigor <igor.padilha2004@gmail.com>
2022-10-20 21:05:53 -0300

    🐛 fix: finish response of utf-8 encoded data

--- src/database/connect.php
+++ src/database/connect.php
@@ -7,7 +7,10 @@
     Dotenv\Dotenv::createImmutable($envFileDir)->load();
 
     function Connect() {
-        return new mysqli($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"], $_ENV["DB_PORT"]);
+        $connection = new mysqli($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"], $_ENV["DB_PORT"]);
+        $connection->query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+
+        return $connection;
     }
 
 ?>
